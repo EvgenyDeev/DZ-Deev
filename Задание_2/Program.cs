9610c99ec8d1621ff0b99e48b1b5bf13655725a9
Evgeny <deevzhenja@inbox.ru>
2022-09-22 17:40:37 +0300

    Сделал задание №2

--- Задание_2/Program.cs
+++ Задание_2/Program.cs
@@ -2,14 +2,18 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
+
 Console.Clear();
-int a = Console.WriteLine("Введите значение a: ");
-int b = Console.WriteLine("Введите значение b: ");
-if (int a > int b)
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
 {
-    Console.WriteLine(int a);
+    Console.WriteLine($"максимальное число {a}, минимальное число {b}");
 }
 else
 {
-    Console.WriteLine(int b);
+    Console.WriteLine($"максимальное число {b}, минимальное число {a}");
 }
